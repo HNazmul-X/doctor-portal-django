596a9ad7f3a1dd16f3b5922ff701db18aa2183c1
hnazmul <nazmul.w3@gmail.com>
2022-04-03 22:40:37 +0600

    everyfile deleted

--- doctorPortal/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import UserModel,PateintFeestatus,PateintModel,DoctorsModel,AppointmentModel
-
-# Register your models here.
-admin.site.register(UserModel)
-admin.site.register(PateintModel)
-admin.site.register(PateintFeestatus)
-admin.site.register(DoctorsModel)
-admin.site.register(AppointmentModel)
